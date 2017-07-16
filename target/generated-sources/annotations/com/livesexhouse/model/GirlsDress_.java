f097c7480600ebf7cdb1404a3e1375520c1fd040
Milan Milosevic <miloshevicmilan@hotmail.com>
2017-07-16 20:58:10 +0200

    new

--- target/generated-sources/annotations/com/livesexhouse/model/GirlsDress_.java
+++ target/generated-sources/annotations/com/livesexhouse/model/GirlsDress_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-15T12:01:52")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-15T12:50:29")
 @StaticMetamodel(GirlsDress.class)
 public class GirlsDress_ { 
 
