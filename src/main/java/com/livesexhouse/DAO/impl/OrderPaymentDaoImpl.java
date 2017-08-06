d6eaa403a0bcc8509b4811a2bb1e44e9fc2aa0ae
nstankovic <nemanjastankovic91@gmail.com>
2017-08-06 10:43:33 +0200

    Epoch controller

--- src/main/java/com/livesexhouse/DAO/impl/OrderPaymentDaoImpl.java
+++ src/main/java/com/livesexhouse/DAO/impl/OrderPaymentDaoImpl.java
@@ -2,7 +2,13 @@ package com.livesexhouse.DAO.impl;
 
 import com.livesexhouse.DAO.OrderPaymentDao;
 import com.livesexhouse.model.OrderPayment;
+import com.livesexhouse.model.PricePackage;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 /**
  *
@@ -15,4 +21,14 @@ public class OrderPaymentDaoImpl extends AbstractGenericDao<OrderPayment> implem
         super(OrderPayment.class);
     }
 
+    @Override
+    public OrderPayment findByTransactionId(String transactionId) {
+        CriteriaBuilder criteriaBuilder = getCurrentSession().getCriteriaBuilder();
+        CriteriaQuery<OrderPayment> criteriaQuery = criteriaBuilder.createQuery(getEntityType());
+        Root<OrderPayment> root = criteriaQuery.from(getEntityType());
+        CriteriaQuery<OrderPayment> queryByTransactionId = criteriaQuery.select(root).
+                where(criteriaBuilder.equal(root.get("transactionId"), transactionId));
+        TypedQuery<OrderPayment> query = getCurrentSession().createQuery(queryByTransactionId);
+        return query.getSingleResult();
+    }
 }
---
+++ src/main/java/com/livesexhouse/controller/epoch/EpochController.java
@@ -0,0 +1,188 @@
+package com.livesexhouse.controller.epoch;
+
+import com.livesexhouse.DAO.UserDao;
+import com.livesexhouse.model.Order;
+import com.livesexhouse.model.OrderPayment;
+import com.livesexhouse.model.Users;
+import com.livesexhouse.service.OrderPaymentService;
+import com.livesexhouse.service.OrderService;
+import com.livesexhouse.util.CryptoUtil;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.InvalidParameterException;
+
+@RestController
+@RequestMapping("epoch")
+public class EpochController {
+
+	/**
+	 * Logger for this class.
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(EpochController.class);
+
+	/**
+	 * Value {@value #POSTBACK_SUCCESSFUL_TRANSACTION} will be received from
+	 * Epoch for successuful transaction.
+	 */
+	private static final String POSTBACK_SUCCESSFUL_TRANSACTION = "Y";
+
+	/**
+	 * Value {@value #DATA_PLUS_CHARRGEBACK_TRANSACTION} will be received from
+	 * Epoch for chargeback transaction.
+	 */
+	private static final String DATA_PLUS_CHARRGEBACK_TRANSACTION = "D";
+
+	/**
+	 * Value {@value #DATA_PLUS_INITIAL_RECURRING_TRANSACTION} will be received
+	 * from Epoch for Standard Initial Recurring Transaction.
+	 */
+	private static final String DATA_PLUS_INITIAL_RECURRING_TRANSACTION = "I";
+
+	/**
+	 * Value {@value #DATA_PLUS_NON_INITIAL_RECURRING_TRANSACTION} will be
+	 * received from Epoch for Standard NonInitial Recurring Transaction.
+	 */
+	private static final String DATA_PLUS_NON_INITIAL_RECURRING_TRANSACTION = "N";
+
+	@Autowired
+	private OrderService orderService;
+
+	@Autowired
+	private OrderPaymentService orderPaymentService;
+
+	@Autowired
+	private UserDao userDao;
+
+	@Value("${billing.epoch.pi.code}")
+	private String epochPiCode;
+
+	/**
+	 * POST /epoch/postback -> calling after user buy tokens, only Epoch is
+	 * executing this method
+	 * 
+	 * @param ans
+	 * @param memberId
+	 * @param transactionId
+	 * @param piCode
+	 * @param orderId
+	 */
+	@RequestMapping(value = "/postback", method = RequestMethod.POST)
+	public void postback(@RequestParam("ans") String ans, @RequestParam("order_id") String memberId,
+			@RequestParam("transaction_id") String transactionId, @RequestParam("pi_code") String piCode,
+			@RequestParam("x_order_id") Long orderId) {
+        LOGGER.info("REST postback request from Epoch memberId: {} transactionId: {} orderId: {}", memberId,
+				transactionId, orderId);
+        try {
+            String decryptedOrderId = CryptoUtil.decrypt(orderId.toString().getBytes());
+            orderId = Long.valueOf(decryptedOrderId);
+            validatePostBackParameters(transactionId, piCode, orderId);
+        } catch (Exception e) {
+			LOGGER.error(e.getMessage());
+		}
+		Order order = getOrderService().findById(orderId);
+		Users user = order.getUser();
+		if (user.getEpochMemberId() == null && memberId != null) {
+			user.setEpochMemberId(memberId);
+		}
+		user.setTokens(user.getTokens() + order.getPricePackage().getTokens());
+		getUserDao().save(user);
+		getOrderPaymentService().create(order, transactionId);
+	}
+
+	private void validatePostBackParameters(String ans, String piCode, Long orderId) throws InvalidParameterException {
+		if (!POSTBACK_SUCCESSFUL_TRANSACTION.equals(ans)) {
+			throw new InvalidParameterException(String.format("Transaction failed answer code: %s ", ans));
+		}
+		if (!StringUtils.equals(piCode, getEpochPiCode())) {
+			throw new InvalidParameterException(
+					String.format("Postback processing failed pi code: %s did not match!", piCode));
+		}
+		if (orderId == null) {
+			throw new InvalidParameterException("Postback processing failed Order id is not provided!");
+		}
+		Order order = getOrderService().findById(orderId);
+		if (order == null) {
+			throw new InvalidParameterException(
+					String.format("Postback processing failed Order id: %d is not correct!", orderId));
+		}
+	}
+
+	/**
+	 * POST /epoch/dataplus -> processing transactions from Epoch (chargebacks,
+	 * recurrings...), only Epoch is executing this method
+	 *
+	 * @param transactionId
+	 * @param originalTransactionId
+	 * @param transactionType
+	 * @param memberId
+	 * @param username
+	 * @param email
+	 * @param transactionDate
+	 */
+	@RequestMapping(value = "/dataplus", method = RequestMethod.POST)
+	public void dataplus(@RequestParam("ets_transaction_id") String transactionId,
+			@RequestParam("ets_ref_trans_ids") String originalTransactionId,
+			@RequestParam("ets_transaction_type") String transactionType,
+			@RequestParam("ets_member_idx") String memberId,
+			@RequestParam(value = "ets_username", required = false) String username,
+			@RequestParam(value = "ets_email", required = false) String email,
+			@RequestParam(value = "ets_transaction_date", required = false) String transactionDate) {
+		LOGGER.info(
+				"REST request from Epoch transactionId: {} originalTransactionId: {} transactionType: {} memberId: {} username: {} transactionDate: {}",
+				transactionId, originalTransactionId, transactionType, memberId, username, transactionDate);
+		switch (transactionType) {
+		case DATA_PLUS_CHARRGEBACK_TRANSACTION:
+			handleChargebackTransaction(originalTransactionId);
+			break;
+		case DATA_PLUS_INITIAL_RECURRING_TRANSACTION:
+		case DATA_PLUS_NON_INITIAL_RECURRING_TRANSACTION:
+			handleRecurringTransaction(memberId, transactionId);
+			break;
+		default:
+			LOGGER.error("Epoch dataplus type not supported!");
+			break;
+		}
+	}
+
+	private void handleChargebackTransaction(String originalTransactionId) {
+		OrderPayment orderPayment = getOrderPaymentService().findByTransactionId(originalTransactionId);
+		if (orderPayment == null) {
+			LOGGER.error("Order Payment for transaction id: {} could not be found!", originalTransactionId);
+		} else {
+			LOGGER.debug("Processing Order Payment with id: {}", orderPayment.getId());
+			Order order = orderPayment.getOrder();
+			Users user = order.getUser();
+			user.setTokens(user.getTokens() - order.getPricePackage().getTokens());
+			getUserDao().save(user);
+		}
+	}
+
+	private void handleRecurringTransaction(String memberId, String transactionId) {
+		// TODO Auto-generated method stub
+	}
+
+	private OrderService getOrderService() {
+		return orderService;
+	}
+
+	private OrderPaymentService getOrderPaymentService() {
+		return orderPaymentService;
+	}
+
+    private UserDao getUserDao() {
+        return userDao;
+    }
+
+    private String getEpochPiCode() {
+		return epochPiCode;
+	}
+
+}
