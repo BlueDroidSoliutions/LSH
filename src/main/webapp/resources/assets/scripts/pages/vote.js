573ae2e5b91d751c8a565b7aa719da81c7220ce4
nstankovic <nemanjastankovic91@gmail.com>
2017-08-06 20:36:37 +0200

    vote.js update

--- src/main/webapp/resources/assets/scripts/pages/vote.js
+++ src/main/webapp/resources/assets/scripts/pages/vote.js
@@ -1,7 +1,9 @@
 function buyTokens(csrfToken, pricePackageId) {
     $.post('/checkout', { _csrf: csrfToken, pricePackageId: pricePackageId })
         .always(function(response) {
-            if (response.success == false) {
+            if (response.success) {
+                window.open(response.redirectUrl, '_self');
+            } else {
                 alert(response.message);
             }
         });
